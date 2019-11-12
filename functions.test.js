c7396330e5c01faa27e85c2c79dad631f9246434
Steven Hanson <steven.hanson@wintechnologies.net>
2019-11-12 10:47:19 +0000

    Add toBeLessThan

--- functions.test.js
+++ functions.test.js
@@ -29,3 +29,9 @@ test('user should be Brad Traversy object', () => {
     });
 });
 
+// less than
+test('should be under 1600', () => {
+    const load1 = 800;
+    const load2 = 700;
+    expect(load1 + load2).toBeLessThan(1600);
+});
