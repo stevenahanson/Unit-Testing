1fdfc9c506d22385797d84d9f8547e4647842fc4
Steven Hanson <steven.hanson@wintechnologies.net>
2019-11-12 10:39:14 +0000

    Add test for isNull

--- functions.js
+++ functions.js
@@ -1,5 +1,6 @@
 const functions = { 
     add: (num1, num2) => num1 + num2,
+    isNull: () => undefined
 };
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,12 @@ const functions = require('./functions');
 test('Adds 2 + 2 to equal 4', () => {
     expect(functions.add(2,2)).toBe(4);
 });
+
+test('Adds 2 + 2 to not equal 5', () => {
+    expect(functions.add(2,2)).not.toBe(5);
+});
+
+test('should be null', () => {
+    expect(functions.isNull()).toBeNull();
+});
+
