a34fd95d68b3b7b2c6878ca850ce89d941b6b222
Steven Hanson <steven.hanson@wintechnologies.net>
2019-11-12 10:45:23 +0000

    Add toEqual

--- functions.js
+++ functions.js
@@ -1,6 +1,12 @@
 const functions = { 
     add: (num1, num2) => num1 + num2,
-    isNull: () => undefined
+    isNull: () => null,
+    checkValue: x => x,
+    createUser: () => {
+        const user = { firstName: 'Brad'}
+        user['lastname'] = 'Traversy';
+        return user;
+    } 
 };
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -1,18 +1,31 @@
 const functions = require('./functions');
 
+// toBe
 test('Adds 2 + 2 to equal 4', () => {
     expect(functions.add(2,2)).toBe(4);
 });
 
+// not.toBe
 test('Adds 2 + 2 to not equal 5', () => {
     expect(functions.add(2,2)).not.toBe(5);
 });
 
+
+// toBeNull
 test('should be null', () => {
     expect(functions.isNull()).toBeNull();
 });
 
+// toBeFalsy
 test('should be falsy', () => {
     expect(functions.checkValue(null)).toBeFalsy();
+});
+
+// toEqual
+test('user should be Brad Traversy object', () => {
+    expect(functions.createUser()).toEqual({ 
+        firstName: 'Brad', 
+        lastname: 'Traversy'
+    });
 });
 
