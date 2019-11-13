5ef7add11fe67788cb30424e2a13129bf8bb06dc
Steven Hanson <steven.hanson@wintechnologies.net>
2019-11-13 12:27:48 +0000

    working with async data test not working

--- functions.js
+++ functions.js
@@ -1,3 +1,6 @@
+// const axios = require('axios');
+// const url = 'https://jsonplaceholder.typicode.com/users/1';
+
 const functions = { 
     add: (num1, num2) => num1 + num2,
     isNull: () => null,
@@ -6,7 +9,12 @@ const functions = {
         const user = { firstName: 'Brad'}
         user['lastname'] = 'Traversy';
         return user;
-    } 
+    },
+    // fetchUser: () => 
+    //     axios
+    //         .get(url)
+    //         .then(res => res.data)
+    //         .catch(err => 'error')
 };
 
 module.exports = functions;
